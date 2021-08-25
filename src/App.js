ff6f04f26818df453cd984634e7db567d1deadbb
AditiiJain <iaditi.jain08@gmail.com>
2021-08-25 23:46:48 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+    return ( <div>
+        <h1 className="heading">GeeksForGeeks</h1>
+    </div> );
+}
+ 
+export default App;
